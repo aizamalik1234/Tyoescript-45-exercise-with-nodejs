401a0626a7e422d1bac921fbf25cfdca9323ddb9
aizamalik1234 <aizamalik6789@gmail.com>
2024-03-07 14:53:08 +0500

    First commit

---
+++ exer24/main.js
@@ -0,0 +1,33 @@
+// Define variables
+var apple = "apple";
+var uppercaseApple = "APPLE";
+var ten = 10;
+var twenty = 20;
+// test for equality and inequality with string
+console.log("is apple is equal tp apple?");
+console.log(apple == "apple");
+console.log("\nis apple is not equal to apple");
+console.log(apple != "apple");
+// test using lowercase funtion
+console.log("is APPLE is equal to apple after converting to lowercase?");
+console.log(uppercaseApple.toLowerCase() == "apple");
+console.log("\n APPLE is not equal to apple after converting to lowercase?");
+console.log(uppercaseApple.toLowerCase() != "apple");
+// numerical tests
+console.log("\n is ten is equal to twenty");
+console.log(ten == twenty);
+// not equal to
+console.log("\n ten is not equal to twenty?");
+console.log(ten != twenty);
+// greater than
+console.log("\n is ten is greater than zero");
+console.log(ten > 0);
+// less than
+console.log("\n is twenty is less than 10");
+console.log(twenty < 10);
+// greater than or equal to
+console.log("\n is ten is greater than or equal to 5?");
+console.log(ten >= 5);
+// less than or equal to
+console.log("\n twenty is less than oe equal to 10");
+console.log(twenty <= 10);
